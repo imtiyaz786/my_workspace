27653042edc1d67929da0db0dc65ad867fab8868
imtiyaz786 <iali75637@gmail.com>
2021-08-02 16:24:58 +0530

    my workspace

--- my_workspace/DomainExtraction.java
+++ my_workspace/DomainExtraction.java
@@ -3,8 +3,9 @@ package my_workspace;
 public class DomainExtraction {
 
 	public static void main(String[] args) {
-		String 	str = "https://app.revature.com";
-		System.out.println(str.replaceAll("[// / :]", "").replace("https", "app.revature.com"));
+		String 	str = "https://en.wikipedia.org/wiki/Main_Page";
+		String str1 = str.replaceAll("[// / :]", "").replace("httpsen.wikipedia.orgwikiMain_Page", "en.wikipedia.org");
+		System.out.println(str1);
 		
 	}
 
